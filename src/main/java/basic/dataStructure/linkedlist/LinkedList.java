3f9f863c1576c331b207107b1bb4a0e821090195
Naisheng Zhang <jonathan.n.zhang@gmail.com>
2020-06-14 13:29:32 -0400

    add linkedlist

---
+++ src/main/java/basic/dataStructure/linkedlist/LinkedList.java
@@ -0,0 +1,148 @@
+package basic.dataStructure.linkedlist;
+
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node dummyhead;
+    private int size;
+
+    public LinkedList() {
+        dummyhead = new Node(null, null);
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("add failed");
+        }
+
+        Node pre = dummyhead;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        pre.next = new Node(e, pre.next);
+        size++;
+
+    }
+
+    public void addFirst(E e) {
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+        //=>
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("get failed");
+        }
+        Node cur = dummyhead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Set failed");
+        }
+        Node cur = dummyhead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyhead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Set failed");
+        }
+        Node pre = dummyhead;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        Node retNode = pre.next;
+        pre.next = retNode.next;
+        retNode.next = null;
+        size--;
+        return retNode.e;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node cur = dummyhead.next;
+        while (cur != null) {
+            sb.append(cur + "->");
+            cur = cur.next;
+        }
+        sb.append("NULL");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+    }
+}
